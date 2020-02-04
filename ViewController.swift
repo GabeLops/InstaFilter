4d1898e91f36c7a7f731324387cb6c4577df7c36
GabeLops <gabrieldlops@gmail.com>
2020-02-03 23:06:10 -0800

    add animation for imageview

--- ViewController.swift
+++ ViewController.swift
@@ -68,6 +68,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true)
         
         currentImage = image
+        imageView.alpha = 0
+        UIView.animate(withDuration: 2.0, delay: 0, options: [], animations: {
+            self.imageView.alpha = 1
+        })
         
         let beginImage = CIImage(image: currentImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
